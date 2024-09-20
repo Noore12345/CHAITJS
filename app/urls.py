936c0735c1e18bdc6188cd26d058765cae8da337
Noore Nabi <noorenabi845304@gmail.com>
2024-09-20 15:28:54 +0530

    first Commit

--- app/urls.py
+++ app/urls.py
@@ -2,16 +2,7 @@ from django.urls import path
 from .views import *
 
 
-urlpatterns=[
-    #  path('google/calendar/init/', GoogleCalendarInitView.as_view(), name='google_calendar_init'),
-    # path('google/calendar/redirect/', GoogleCalendarRedirectView.as_view(), name='google_calendar_redirect'),
-    # path('abcd/',InterviewQuestionView),
-    # path('abcd/<int:pk>/',InterviewQuestionView),
-    path('google/forms/init/', GoogleFormsInitView.as_view(), name='google_forms_init'),
-    path('google/forms/redirect/', GoogleFormsRedirectView.as_view(), name='google_forms_redirect'),
-    # path('google/forms/access/<uuid:token>/', AccessFormView.as_view(), name='access_form'),
-     path('api/sales-data/', SalesData.as_view(), name='sales-data'),
-    #  path('sales-chart/', sales_chart, name='sales-chart'),
+urlpatterns=[ 
      path('company/',comapnyviews)
  
 ]
--- app/views.py
+++ app/views.py
@@ -1,36 +1,7 @@
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import Flow
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
-from django.http import JsonResponse, HttpResponseRedirect
-from rest_framework.views import APIView
-from rest_framework import status
-import uuid
-import logging
 from .models import *
 from .serializers import *
 from rest_framework.decorators import api_view
-
-logger = logging.getLogger(__name__)
-
-
-# views.py
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
-class SalesData(APIView):
-    def get(self, request, format=None):
-        data = {
-            "labels": ["January", "February", "March", "April", "May"],
-            "sales": [100, 200, 150, 300, 250]
-        }
-        return Response(data)
-
-# views.py
-# from django.shortcuts import render
-
-# def sales_chart(request):
-#     return render(request, 'sales_chart.html')
 
 
 @api_view(['GET'])
@@ -39,7 +10,3 @@ def comapnyviews(request):
     serializers=CompanySerializer(company_obj,many=True)
     return Response(serializers.data)
 
-# from django.shortcuts import render
-
-# def sales_chart(request):
-#     return render(request, 'sales_chart.html')
